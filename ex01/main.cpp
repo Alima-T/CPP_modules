2a349a3c9bf5145557a84e977341a7dc86fba6e5
Alimat Okhapkina <aokhapki@2-A-6.42heilbronn.de>
2025-05-06 16:52:15 +0200

    add functions in PoneBook and main in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,27 @@
 /*   By: aokhapki <aokhapki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 15:22:30 by aokhapki          #+#    #+#             */
-/*   Updated: 2025/05/06 15:22:31 by aokhapki         ###   ########.fr       */
+/*   Updated: 2025/05/06 16:47:26 by aokhapki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+#include <iostream>
+
+int main() {
+	// PhoneBook phonebook;
+	// std::string command;
+
+	// while (true) {
+	// 	std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+	// 	std::getline(std::cin, command);
+
+	// 	if (command == "ADD")
+	// 		phonebook.addContact();
+	// 	else if (command == "SEARCH")
+	// 		phonebook.searchContacts();
+	// 	else if (command == "EXIT")
+	// 		break;
+	// }
+	return 0;
+}
